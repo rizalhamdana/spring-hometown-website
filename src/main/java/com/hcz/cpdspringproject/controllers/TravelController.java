95f903366f96876cfa94817057fe25558610d783
Agung Ramadhan Putra <agung.mizor@gmail.com>
2019-12-23 13:51:31 +0800

    travel UI dan controller

---
+++ src/main/java/com/hcz/cpdspringproject/controllers/TravelController.java
@@ -0,0 +1,27 @@
+package com.hcz.cpdspringproject.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hcz.cpdspringproject.pojo.Travel;
+import com.hcz.cpdspringproject.service.TravelService;
+
+@Controller
+public class TravelController {
+	
+	@Autowired
+    private TravelService travelService;
+
+    @RequestMapping("/travels")
+    public String index(Model model) {
+    	List<Travel> allTravels = travelService.getAllTravels();
+        model.addAttribute("currentTravels", allTravels);
+        return "travel";
+    }
+
+
+}
