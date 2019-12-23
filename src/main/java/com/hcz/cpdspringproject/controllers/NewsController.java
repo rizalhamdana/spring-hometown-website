46f4694920ce7d1ca5dcdaa4e27f99b343f81053
rizalhamdana <ari.gusti12@gmail.com>
2019-12-23 13:47:37 +0800

    news controller has finished, but still need to check

--- src/main/java/com/hcz/cpdspringproject/controllers/NewsController.java
+++ src/main/java/com/hcz/cpdspringproject/controllers/NewsController.java
@@ -39,8 +39,8 @@ public class NewsController {
         }
     }
 
-    @RequestMapping("/admin/news")
-    public String newsDetail(Model model) {
+    @RequestMapping("/admin/all-news")
+    public String adminGetAllNews(Model model) {
         List<News> allNews = newsService.getAllNews();
         if (allNews != null) {
             model.addAttribute("allNews", allNews);
@@ -50,14 +50,51 @@ public class NewsController {
         }
     }
 
-    @RequestMapping(value = "admin/news", method = RequestMethod.POST)
-    public String insertNews(@ModelAttribute("news") News news) {
+    @RequestMapping("/admin/add-news")
+    public String showAddNewsForm(Model model) {
+        News news = new News();
+        model.addAttribute("news", news);
+        return "admin_add_news_form";
+    }
+
+    @RequestMapping(value = "/admin/news", method = RequestMethod.GET)
+    public String adminDetailNews(@RequestParam("news_id") int newsId, Model model) {
+        News news = newsService.getNewsById(newsId);
+        if (news != null) {
+            model.addAttribute("news", news);
+            return "admin_update_news_form";
+        } else {
+            return "error_404";
+        }
+    }
+
+    @RequestMapping(value = "/admin/news", method = RequestMethod.PUT)
+    public String adminUpdateNews(@ModelAttribute("news") News news) {
+        int updateNews = newsService.updateNews(news);
+        if (updateNews > 0) {
+            return "redirect:/admin/all-news";
+        } else {
+            return "error_400";
+        }
+    }
+
+    @RequestMapping(value = "/admin/news", method = RequestMethod.POST)
+    public String adminInsertNews(@ModelAttribute("news") News news) {
         int insertNews = newsService.addNews(news);
         if (insertNews > 0) {
             return "redirect:admin/news";
         } else {
-            return "error_500";
+            return "error_400";
         }
+    }
 
+    @RequestMapping(value = "/admin/news", method = RequestMethod.DELETE)
+    public String adminDeleteNews(@RequestParam("news_id") int newsId) {
+        int deletedNews = newsService.deleteNews(newsId);
+        if (deletedNews > 0) {
+            return "redirect:admin/news";
+        } else {
+            return "error_400";
+        }
     }
 }
